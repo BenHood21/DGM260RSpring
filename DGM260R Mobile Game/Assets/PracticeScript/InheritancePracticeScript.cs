3495ac125e1a8434a2dd5efcb95739ed6a1d7e1e
Benjamin Hood <10885066@my.uvu.edu>
2022-01-12 16:43:23 -0700

    Practice script added

---
+++ DGM260R Mobile Game/Assets/PracticeScript/InheritancePracticeScript.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class InheritancePracticeScript : MonoBehaviour
+{
+    class MainClass
+    {
+        class Hero
+        {
+            public string name;
+            public string weapon;
+            public float goal;
+
+            public void PrintBaseValues()
+            {
+                Console.WriteLine("Name: " + name);
+                Console.WriteLine("Weapon: " + weapon);
+                Console.WriteLine("Goal: " + goal);
+            }
+        }
+
+        class Archer : Hero
+        {
+            public int arrowCount;
+
+            public void Attack()
+            {
+                Console.WriteLine("Fire");
+            }
+        }
+
+        class Warrior : Hero
+        {
+            public Color hairColor;
+
+            public void WarCry()
+            {
+                Console.WriteLine("AHHHHHH");
+            }
+        }
+
+        public void Main(string[] args)
+        {
+            Warrior Tank = new Warrior();
+            Tank.name = "Martty";
+            Tank.weapon = "Axe";
+            Tank.goal = 50f;
+            Tank.hairColor = Color.gray;
+            Tank.PrintBaseValues();
+            Tank.WarCry();
+        }
+    }
+}
+
+
