4251e202b6592607d29281860a36ccef8fb1234b
Benjamin Hood <10885066@my.uvu.edu>
2022-02-03 17:12:25 -0700

    Horizontal movement working UI!

---
+++ DGM260RMoblieGame1/Assets/Script/PlayerMobileScript.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMobileScript : MonoBehaviour
+{
+  public float playerSpeed;
+  public Joystick joystick;
+  public float gravity = -9.81f;
+  public float jumpHeight = 3f;
+  public Transform groundCheck;
+  public float groundDistance = 0.4f;
+  public LayerMask groundMask;
+  public bool isGround;
+
+  private float horizontalMove = 5f;
+  private float verticalMove = 5f;
+  private Rigidbody rb;
+  private Vector3 playerDirection;
+  private Vector3 velocity;
+    
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  void Update()
+
+  {
+    // Youtube Tutorial: TOUCH CONTROLS in Unity! by Brackeys
+
+    if (joystick.Vertical >= .2f)
+    {
+      verticalMove = playerSpeed;
+      Debug.Log("Front Movement");
+    }
+    if (joystick.Vertical <= -.2f)
+    {
+      verticalMove = -playerSpeed;
+      Debug.Log("Back Movement");
+    }
+    if (joystick.Horizontal >= .2f)
+    {
+      horizontalMove = playerSpeed;
+      Debug.Log("Right Movement");
+    }
+    else if (joystick.Horizontal <= -.2f)
+    {
+      horizontalMove = -playerSpeed;
+      Debug.Log("Left Movement");
+    }
+    else
+    {
+      horizontalMove = 0f;
+      verticalMove = 0f;
+    }
+
+    playerDirection = new Vector3(horizontalMove, 0, verticalMove).normalized;
+  }
+
+  void FixedUpdate()
+  {
+    rb.velocity = new Vector3(playerDirection.x * playerSpeed, 0, playerDirection.z);
+
+
+    // Movement Controls and detection
+    isGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+    velocity.y += gravity * Time.deltaTime;
+
+    if (isGround && velocity.y < 0)
+    {
+      velocity.y = -2f;
+      Debug.Log("On Ground");
+    }
+
+    if (Input.GetButtonDown("Jump") && isGround)
+    {
+      velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+    }
+  }
+} 
+
+ 
+
